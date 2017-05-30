bffef1f45f29efe82d52bb459e91cd208a326928
chunggilee <chungyi347@gmail.com>
2017-05-30 20:24:51 +0900

    fix problem

--- InCook/__init__.py
+++ InCook/__init__.py
@@ -1,12 +1,2 @@
 import pymysql
 pymysql.install_as_MySQLdb()
-
-from InCook.models import *
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from django.core.urlresolvers import reverse
-from django.contrib.contenttypes.models import ContentType
-from django.conf import settings
-from django.db.models import Max, Count, Q
-import json
-
