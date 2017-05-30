ced7cced02e6930e6d6bcf8bd01a36ded3294151
Heena Kwag <dotaitch@gmail.com>
2017-05-31 05:12:08 +0900

    Account, Ingredient, Recipe models

---
+++ recipe/choices.py
@@ -0,0 +1,32 @@
+HEALTH_LABELS = (
+    ('Tree-Nut-Free', 'Tree-Nut-Free'),
+    ('Paleo', 'Paleo'),
+    ('Vegan', 'Vegan'),
+    ('Gluten-Free', 'Gluten-Free'),
+    ('Peanut-Free', 'Peanut-Free'),
+    ('Vegetarian', 'Vegetarian'),
+    ('Fish-Free', 'Fish-Free'),
+    ('Egg-Free', 'Egg-Free'),
+    ('Shellfish-Free', 'Shellfish-Free'),
+    ('Dairy-Free', 'Dairy-Free'),
+    ('Soy-Free', 'Soy-Free'),
+)
+
+DIET_LABELS = (
+    ('Low-Carb', 'Low-Carb'),
+    ('High-Fiber', 'High-Fiber'),
+    ('Low-Fat', 'Low-Fat'),
+    ('High-Protein', 'High-Protein'),
+    ('Low-Sodium', 'Low-Sodium'),
+    ('Balanced', 'Balanced'),
+)
+
+CAUTIONS = (
+    ('Gluten', 'Gluten'), 
+    ('Soy', 'Soy'),
+    ('Milk', 'Milk'),
+    ('Wheat', 'Wheat'),
+    ('Tree-Nuts', 'Tree-Nuts'),
+    ('Eggs', 'Eggs'),
+    ('Shellfish', 'Shellfish'),       
+)
