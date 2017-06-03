86e0a1301093b8e635729b0212932a6665426481
PR4DES <qwe52165@gmail.com>
2017-06-03 09:33:46 +0900

    url

---
+++ InCook/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from recipe import views as recipe_views
+from . import views as incook_views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'InCook.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', incook_views.main,  name='main'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
+    url(r'^recipe/$', recipe_views.get_recipe,  name='get_recipe'),
+    url(r'^like/$', recipe_views.add_like,  name='add_like'),
+    url(r'^bookmark/$', recipe_views.add_bookmark,  name='add_bookmark'),
+    url(r'^add/$', recipe_views.add_recipe, name='add_recipe'),
+    url(r'^rating/$', recipe_views.add_rating,  name='add_rating'),
+    url(r'^search/$', recipe_views.search,  name='search'),
+]
