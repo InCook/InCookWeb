a745722c2f6dceaf1bb85774d433e4a0a6752dd8
ChunggiLee <9670425@naver.com>
2017-06-09 14:34:32 +0900

    refri

--- InCook/urls.py
+++ InCook/urls.py
@@ -10,9 +10,10 @@ urlpatterns = [
     # url(r'^$', 'InCook.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', incook_views.main,  name='main'),
+    #url(r'^$', incook_views.main,  name=''),
+    url(r'^main$', recipe_views.test, name='main'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
+    url(r'^', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
 	url(r'^profile$', account_views.profile, name='profile'),
     url(r'^recipe/$', recipe_views.get_recipe,  name='get_recipe'),
     url(r'^like/$', recipe_views.add_like,  name='add_like'),
